bac2ca23cb01078a349cd333d9c634c94dccb142
lascaranzas <ezequielrdsv@gmail.com>
2020-08-12 12:39:37 -0300

    nueva actualizacion

--- js/login.js
+++ js/login.js
@@ -1,3 +1,14 @@
+function validar(){
+var usuario = document.getElementById("usuario"). Value;
+var contraseña = document.getElementById("contraseña"). Value;
+if (usuario=="jovenes"&& contraseña =="1234") {
+   alert("datos ingresados correctamente")
+}
+else{
+    alert("error de validacion ")
+}
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
